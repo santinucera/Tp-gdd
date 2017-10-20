6928aa0b595c97269dbb6fad1fdd2cc9e60abd17
santinucera <santinucera4@gmail.com>
2017-10-19 23:19:59 -0300

    Agrego script para borrar tablas

---
+++ LimpiarTodo.sql
@@ -0,0 +1,17 @@
+DROP TABLE gd_esquema.RegistroFactura
+DROP TABLE gd_esquema.Registro
+DROP TABLE gd_esquema.FuncionalidadRol
+DROP TABLE gd_esquema.UsuarioRol
+DROP TABLE gd_esquema.Rol
+DROP TABLE gd_esquema.Funcionalidad
+DROP TABLE gd_esquema.UsuarioSucursal
+DROP TABLE gd_esquema.Usuario
+DROP TABLE gd_esquema.ItemFactura
+DROP TABLE gd_esquema.Factura
+DROP TABLE gd_esquema.Rubro
+DROP TABLE gd_esquema.Rendicion
+DROP TABLE gd_esquema.Sucursal
+DROP TABLE gd_esquema.MedioPago
+DROP TABLE gd_esquema.Empresa
+DROP TABLE gd_esquema.Cliente
+DROP TABLE gd_esquema.Devolucion
